f4de1692078aec50dfabd61fa6310f5ba82de7cf
Alexey Arhipiv <alex3171@list.ru>
2020-08-07 15:19:54 +0500

    Домашнее задание lesson02

--- scrypt/scrypt.js
+++ scrypt/scrypt.js
@@ -6,9 +6,9 @@ for (let i = 0; i < num.length; i++) {
   result *= +num[i];
 }
 console.log("умножение цифр = " + result);
-result **= 3;
+result **= 3; //Возведение в степень 3
 
 console.log(
-  "Первые две цифры при возведении в 3ю степень =  " +
+  "Первые две цифры при возведении числа '1296' в степень 3 =  " +
     +result.toString().substring(0, 2)
 );
