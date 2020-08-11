d7b4f1c2a548df759d96e25f5f006d52071fb739
Alexey Arhipiv <alex3171@list.ru>
2020-08-12 00:20:40 +0500

    fix: home work

--- scrypt/scrypt.js
+++ scrypt/scrypt.js
@@ -1,17 +1,15 @@
 "use strict";
-let str = prompt('Введите строку', 'Сегодня хорошая погода, но скоро осень');
-
+let str = prompt("Введите строку", "Сегодня хорошая погода, но скоро осень");
 
 function strTrim(s) {
-  if (typeof s === 'string') {
+  if (typeof s === "string" && isNaN(s)) {
     if (s.length > 30) {
-      console.log(s.trim().substr(0, 30) + '...');
+      console.log(s.trim().substr(0, 30) + "...");
     } else {
       console.log(s.trim());
     }
   } else {
-    alert('Вы ввели не строку');
+    alert("Вы ввели не строку");
   }
-
-};
+}
 strTrim(str);
