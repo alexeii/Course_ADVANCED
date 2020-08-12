2688db34acd8cf1d134fcb2f78618871a221d39f
Alexey Arhipiv <alex3171@list.ru>
2020-08-12 14:18:16 +0500

    feat: Home work

--- scrypt/scrypt.js
+++ scrypt/scrypt.js
@@ -1,15 +1,26 @@
 "use strict";
-let str = prompt("Введите строку", "Сегодня хорошая погода, но скоро осень");
-
-function strTrim(s) {
-  if (typeof s === "string" && isNaN(s)) {
-    if (s.length > 30) {
-      console.log(s.trim().substr(0, 30) + "...");
-    } else {
-      console.log(s.trim());
-    }
-  } else {
-    alert("Вы ввели не строку");
-  }
+//1 задача
+let arr = [
+  "255687",
+  "152845",
+  "4584785",
+  "1234567",
+  "2456984",
+  "1597536",
+  "4789654",
+];
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i][0] === "2" || arr[i][0] === "4") console.log(arr[i]);
 }
-strTrim(str);
+
+//2 задача
+for (let i = 2; i < 100; i++) {
+  let count = 0;
+  for (let j = 1; j <= i; j++) {
+    if (i % j) {
+      continue;
+    }
+    count += 1;
+  }
+  if (count === 2) console.log(i + " :Делители этого числа: 1 и " + i);
+}
